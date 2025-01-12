c133be12deff4eadd99b3f63f302515ab7e9e767
Anjali Raja <anjaliraja1406@gmail.com>
2025-01-12 20:31:03 +0530

    add codes

---
+++ make_newfile.py
@@ -0,0 +1,27 @@
+
+st="This code will make new file in that folder"
+f=open("new_file.txt","a")
+f.write(st)
+f.close()
+
+'''
+append the line
+
+This code will make new file in that folderThis code will make new file in that folderThis code will make new file in that folderThis code will make new file in that folderThis code will make new file in that folder
+
+'''
+
+# st="This code will make new file in that folder"
+# f=open("new_file.txt","w")
+# f.write(st)
+# f.close()
+
+'''
+output-
+
+the new file couls be made as new_file.txt
+
+A Data Engineer is a professional who designs, builds, and maintains the infrastructure and pipelines that enable the collection, storage, and processing of large volumes of data. They ensure data is clean, reliable, and accessible for analysis and decision-making. Skilled in programming, database management, and cloud technologies, Data Engineers play a crucial role in supporting data scientists, analysts, and business operations by transforming raw data into usable formats for insights and applications.
+
+
+'''
---
+++ read_lines.py
@@ -0,0 +1,4 @@
+f=open("file1.txt")
+lines=f.readlines()
+print(lines)
+print(type(lines))
