4177b0b9292fa8b8610db8cd7eb0ff0419e249f4
Anjali Raja <anjaliraja1406@gmail.com>
2025-01-29 21:51:35 +0530

    add codes

---
+++ cal_1.py
@@ -0,0 +1,17 @@
+# from cal import *
+# print(add(1,2))
+# print(subtract(1,2))
+
+# import cal
+# print(cal.add(1,2))
+# print(cal.subtract(1,2))
+
+
+# from .cal import add 
+# print(add(1,2))
+
+# from cal import add as a
+# print(a(1,3))
+ 
+import cal as c
+print(c.add(2,5))
